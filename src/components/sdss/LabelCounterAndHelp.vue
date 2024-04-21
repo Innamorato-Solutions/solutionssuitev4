c34a32ebf8f1c4ddb604b48803f848b980571ed7
XIIREX <40861116+XIIREX@users.noreply.github.com>
2024-04-21 17:16:52 +0200

    added new components and manual gen view

---
+++ src/components/sdss/LabelCounterAndHelp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="" class="grid grid-cols-3 justify-between">
+    <div class=""> 667 / 60000 Labels</div>
+    <div></div>
+    <Button class="justify-self-end" style="height: 35px;" size="large" icon="pi pi-question-circle" severity="info"
+      raised>
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n(); // use as global scope
+
+</script>
